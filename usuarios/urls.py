2944ad292d318c82b8f7089b2a7952240de547b2
Joao Lucas Goncalves <joaolucasgc@icloud.com>
2025-02-26 20:17:07 -0300

    ✨ feat: Adiciona funcionalidades extras do crud.
    - Esse codigo será explicado amanha 27/2.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import register_view, list_view, detail_view
+from .views import register_view, list_view, detail_view, delete_view, update_view, home_view
 
 app_name = 'usuarios'
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('criar/', register_view, name='criar'),
     path('listar/', list_view, name='listar'),
     path('detalhe/<int:pk>/', detail_view, name='detalhe'),
+    path('atualizar/<int:pk>/', update_view, name='atualizar'),
+    path('deletar/<int:pk>/', delete_view, name='deletar'), 
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Usuario
 from .forms import UsuarioForm
 
+def home_view(request):
+    return render(request, 'usuarios/home.html')
+
 def register_view(request):
     if request.method == 'GET':
         form = UsuarioForm()
@@ -19,11 +22,33 @@ def list_view(request):
         return render(request, 'usuarios/list.html', {'usuarios': usuarios})
     return render(request, 'usuarios/list.html')
 
+
 def detail_view(request, pk):
     usuario = Usuario.objects.get(pk = pk)
     if usuario:
         return render(request, 'usuarios/detail.html', {'usuario': usuario})
     
+
+# Essas duas views functions nos veremos amanhã no inicio da aula
+def delete_view(request, pk):
+    usuario = Usuario.objects.get(pk = pk)
+    if usuario:
+        usuario.delete()
+        #  204 No Content Explicação: O servidor processou o pedido com sucesso, mas não devolveu nenhum conteúdo.
+        request.status_code = 204
+        return redirect('usuarios:listar')
+    
+
+def update_view(request, pk):
+    usuario = Usuario.objects.get(pk = pk)
+    if request.method == 'GET':
+        form = UsuarioForm(instance = usuario)
+        return render(request, 'usuarios/update.html', {'usuario': usuario, 'form': form})
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST, instance = usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('usuarios:listar')
     
                 
         
